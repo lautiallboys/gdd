f01693cc1e520e951a8b6ca4e84a9034727b2ea4
gonziesc <gonziesc@gmail.com>
2017-10-15 19:46:48 -0300

    adding db connection and example

---
+++ tp/src/PagoAgilFrba/DBConnection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace PagoAgilFrba
+{
+     public sealed class DBConnection
+    {
+        string server = ConfigurationManager.AppSettings["server"].ToString();
+        string user = ConfigurationManager.AppSettings["user"].ToString();
+        string password = ConfigurationManager.AppSettings["password"].ToString();
+
+        private static readonly DBConnection instance = new DBConnection();
+
+        private DBConnection() { }
+
+        public static DBConnection getInstance()
+        {
+            return instance;
+        }
+
+        public SqlConnection getConnection()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "SERVER=" + server + "\\SQLSERVER2012;DATABASE=GD2C2017;UID=" + user + ";PASSWORD=" + password + ";";
+            return con;
+        }
+    }
+}
--- tp/src/PagoAgilFrba/Form1.cs
+++ tp/src/PagoAgilFrba/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,23 @@ namespace PagoAgilFrba
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var connection = DBConnection.getInstance().getConnection();
+            SqlCommand command = new SqlCommand("select * from gd_esquema.Maestra;", connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            if (!reader.HasRows)  // El usuario no existe
+                MessageBox.Show("no se que mierda paso");
+            else
+                while (reader.Read())
+                {
+                    MessageBox.Show(reader["Cliente-Apellido"].ToString());
+                }
+            reader.Close();
+            connection.Close();
         }
     }
 }
